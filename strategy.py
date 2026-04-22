b90cf21a475dd4b67163d8b174e8ca568382cead
Dogen <setratosfer@gmail.com>
2026-04-23 03:45:40 +0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,13 +1,14 @@
 from config import CONFIG
 
+
 def calculate_prices(oracle):
     buy_price = oracle * (1 + CONFIG["buy_offset"])
     sell_price = oracle * (1 + CONFIG["sell_offset"])
     return buy_price, sell_price
 
 
-def validate(buy_price, sell_price):
-    return buy_price < sell_price
+def validate(buy, sell):
+    return buy < sell
 
 
 def should_trade(market, oracle):
